66d419de73cd2b3cb280d0ff84444b18de44b292
teapot660 <eeycw9@nottingham.ac.uk>
2020-05-01 13:37:39 +0700

    build commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/chawi/Desktop/Work/Computing Project/20208742/Worksheets/Worksheet 7
+# Build directory: C:/Users/chawi/Desktop/Work/Computing Project/20208742/Worksheets/Worksheet 7/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test1 "test1")
+set_tests_properties(test1 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/chawi/Desktop/Work/Computing Project/20208742/Worksheets/Worksheet 7/CMakeLists.txt;19;add_test;C:/Users/chawi/Desktop/Work/Computing Project/20208742/Worksheets/Worksheet 7/CMakeLists.txt;0;")
